582840e56ae099ebda984b9a32d7eed50f7cb049
NicoRs96 <56675649+NicoRs96@users.noreply.github.com>
2021-09-09 11:22:09 +0200

    prova

---
+++ src/calcolatrice.java
@@ -0,0 +1,19 @@
+
+
+import javax.swing.JFrame;
+
+public class calcolatrice {
+	 public static void main(String[] args){
+	        CalcolatriceFinestra calcolatriceFinestra = new CalcolatriceFinestra("Calcolatrice");
+	        calcolatriceFinestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        calcolatriceFinestra.setSize(300,300);
+	        calcolatriceFinestra.setVisible(true);        
+	    }
+	}
+
+
+
+
+
+
+
